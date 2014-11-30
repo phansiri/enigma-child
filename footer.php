6e12e6982ad361814e25fb5b9fa316e6b2466bc8
phansiri <phansiri@uw.edu>
2014-11-29 21:53:33 -0800

    test for header.php

--- footer.php
+++ footer.php
@@ -10,7 +10,7 @@
 			} else 
 			{ 
 			$args = array(
-			'before_widget' => '<div class="col-md-6 col-sm-6 enigma_footer_widget_column">',
+			'before_widget' => '<div class="col-md-3 col-sm-6 enigma_footer_widget_column">',
 			'after_widget'  => '</div>',
 			'before_title'  => '<h3 class="enigma_footer_widget_title">',
 			'after_title'   => '<div id="" class="enigma-footer-separator"></div></h3>' );
