d4aecdf3af375c01dbee17808580cc1c270a6930
phansiri <phansiri@uw.edu>
2014-11-30 00:39:45 -0800

    added footer function

--- footer.php
+++ footer.php
@@ -5,18 +5,18 @@
 	<div class="container">
 		<div class="row">
 			<?php
-//            if (dynamic_sidebar('new_footer_widget_area')): else : endif;
-			if ( is_active_sidebar( 'footer-widget-area' ) ){
-				dynamic_sidebar( 'footer-widget-area' );
-			} else
-			{
-			$args = array(
-			'before_widget' => '<div class="col-md-3 col-sm-6 enigma_footer_widget_column">',
-			'after_widget'  => '</div>',
-			'before_title'  => '<h3 class="enigma_footer_widget_title">',
-			'after_title'   => '<div id="" class="enigma-footer-separator"></div></h3>' );
-			the_widget('WP_Widget_Pages', null, $args);
-			} ?>
+            if (dynamic_sidebar('new_footer_widget_area')): else : endif;
+//			if ( is_active_sidebar( 'footer-widget-area' ) ){
+//				dynamic_sidebar( 'footer-widget-area' );
+//			} else
+//			{
+//			$args = array(
+//			'before_widget' => '<div class="col-md-3 col-sm-6 enigma_footer_widget_column">',
+//			'after_widget'  => '</div>',
+//			'before_title'  => '<h3 class="enigma_footer_widget_title">',
+//			'after_title'   => '<div id="" class="enigma-footer-separator"></div></h3>' );
+//			the_widget('WP_Widget_Pages', null, $args);
+//			} ?>
 		</div>		
 	</div>
 	
