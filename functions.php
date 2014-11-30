87c2ae7eca3a0f2415e8724757a47aed1d3f904b
phansiri <phansiri@uw.edu>
2014-11-30 00:02:02 -0800

    added new footer widget

--- functions.php
+++ functions.php
@@ -1,5 +1,22 @@
 <?php
 
+//function enigma_child_scripts() {
+//    wp_enqueue_script('extra js', get_stylesheet_directory_uri() . '/js/extra.js');
+//}
+//
+//add_action('wp_enqueue_scripts', 'enigma_child_scripts');
 
+function enigma_child_widgets_init() {
+    register_sidebar(
+        array(
+            'name' => 'New Footer Widget Area',
+            'id' => 'new_footer_widget_area',
+            'before_widget' => '<div class="col-md-8 col-sm-6 enigma_footer_widget_column">',
+            'after_widget' => '</div>',
+            'before_title'  => '<h3 class="enigma_footer_widget_title">',
+            'after_title'   => '<div id="" class="enigma-footer-separator"></div></h3>'
+        )
+    );
+}
 
-?>
+add_action('widgets_init', 'enigma_child_widgets_init');
---
+++ js/extra.js
@@ -0,0 +1,5 @@
+/**
+ * Created by Phansiri on 11/29/14.
+ */
+
+alert('hi');
